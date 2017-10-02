96a7110c0beb01185e4072566c8f9ae9d5db3198
Кирилл Мащенко <kirill010399@mail.ru>
2017-10-02 22:10:33 +0300

    xcode settings

---
+++ MSU_INFO/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MSU_INFO
+//
+//  Created by Кирилл Мащенко on 02.10.2017.
+//  Copyright © 2017 Кирилл Мащенко. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
