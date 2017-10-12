30a561ed655a12995567eed290dcb67577ad24b4
Кирилл Мащенко <kirill010399@mail.ru>
2017-10-12 19:22:56 +0300

    TwoStacks Class added

--- MSU_INFO/TwoStacks.cpp
+++ MSU_INFO/TwoStacks.cpp
@@ -1,5 +1,6 @@
 #include "TwoStacks.h"
 #include <string>
+#include <iostream>
 
 using namespace std;
 
@@ -10,10 +11,14 @@ TwoStacks::TwoStacks()
 
 TwoStacks::TwoStacks(int maxSize)
 {
-    this->array = new char[maxSize];
-    this->curIndex1 = 0;
-    this->curIndex2 = maxSize-1;
-    this->size = maxSize-1;
+    array = new char[maxSize];
+    
+    for (int i=0; i<maxSize; ++i)
+        array[i] = '0'+i%10;
+    
+    curIndex1 = -1;
+    curIndex2 = maxSize;
+    size = maxSize;
 }
 
 TwoStacks::~TwoStacks()
@@ -21,10 +26,9 @@ TwoStacks::~TwoStacks()
     delete [] array;
 }
 
-//functions
 void TwoStacks::stackNull()
 {
-    this->curIndex1 = 0;
+    this->curIndex1 = -1;
     this->curIndex2 = this->size;
 }
 
@@ -34,7 +38,7 @@ int TwoStacks::isNull(int number)
     {
     case 1:
     {
-        return (this->curIndex1 == 0);
+        return (this->curIndex1 == -1);
     }
     case 2:
     {
@@ -46,13 +50,19 @@ int TwoStacks::isNull(int number)
 
 void TwoStacks::addElement (string str, int number)
 {
+    cout<<str<<endl;
     int index;
-    if (number==1) index = this->curIndex1;
-    else index = this->curIndex2;
-
+    
+    if (number == 1)
+        index = ++curIndex1;
+    else
+        index = --curIndex2;
+    
+    cout<<index<<endl;
+    
     for (int i = 0; i < str.size(); ++i)
     {
-        this->array[index] = str[i];
+        array[index] = str[i];
         if (number==1)
         {
             ++this->curIndex1;
@@ -64,84 +74,152 @@ void TwoStacks::addElement (string str, int number)
             --index;
         }
     }
-    this->array[index] = '\0';
-    if (number==1) ++this->curIndex1;
-    else --this->curIndex2;
+    array[index] = '\0';
+    
+    for (int i = 0; i < size; ++i)
+    {
+        if (array[i]=='\0')
+            cout<<' ';
+        else
+            cout<<array[i];
+    }
+    cout<<endl;
+    
+    cout<<curIndex1<<' '<<curIndex2<<endl;
 }
 
 string TwoStacks::getElement (int number)
 {
-    string res;
+    string res = "";
+    
     if (number==1)
     {
-        while (this->array[this->curIndex1]!='\0')
+        if (curIndex1==-1)
+            throw (false);
+        
+        curIndex1--;
+        
+        while (array[curIndex1]!='\0')
         {
-            res+= this->array[this->curIndex1];
-            this->curIndex1--;
+            res+= array[curIndex1];
+            curIndex1--;
+            
+            if (curIndex1 == -1)
+                break;
         }
-        this->curIndex1--;
     }
     else
     {
-        while (this->array[this->curIndex2]!='\0')
+        if (curIndex2==-size)
+            throw (false);
+     
+        curIndex2++;
+        
+        while (array[curIndex2]!='\0')
         {
-            res += this->array[this->curIndex2];
-            this->curIndex2++;
+            res += array[curIndex2];
+            curIndex2++;
+            
+            if (curIndex2 == size)
+                break;
         }
-        this->curIndex2++;
     }
+    
+    cout<<curIndex1<<' '<<curIndex2<<endl;
+    
+    cout<<res<<endl;
+    
     string result;
 
-    for (int i = res.size()-1; i>=0; --i)
+    for (int i = res.size()-1; i >= 0; --i)
         result += res[i];
-
+    
+    cout<<result<<endl; 
+    
     return result;
 }
+
+
 string TwoStacks::getVertice (int number)
 {
-    string res;
+    string res = "";
+    int index;
+    
     if (number==1)
     {
-        int index = this->curIndex1;
-        while (this->array[index]!='\0')
+        if (curIndex1==-1)
+            throw (false);
+        
+        index = curIndex1;
+        
+        index--;
+        
+        while (array[index]!='\0')
         {
-            res+= this->array[index];
+            res+= array[index];
             index--;
+            
+            if (index == -1)
+                break;
         }
     }
     else
     {
-        int index = this->curIndex2;
-        while (this->array[index]!='\0')
+        if (curIndex2==-size)
+            throw (false);
+        
+        index = curIndex2;
+        
+        index++;
+        
+        while (array[index]!='\0')
         {
-            res += this->array[index];
+            res += array[index];
             index++;
+            
+            if (index == size)
+                break;
         }
     }
+    
+    cout<<curIndex1<<' '<<curIndex2<<endl;
+    
+    cout<<res<<endl;
+    
     string result;
-
-    for (int i = res.size()-1; i>=0; --i)
+    
+    for (int i = res.size()-1; i >= 0; --i)
         result += res[i];
-
+    
+    cout<<result<<endl;
+    
     return result;
 }
 
 void TwoStacks::deleteVertice (int number)
-{
+{    
     if (number==1)
     {
-        while (this->array[this->curIndex1]!='\0')
+        curIndex1--;
+        
+        while (array[curIndex1]!='\0')
         {
-            this->curIndex1--;
+            curIndex1--;
+            
+            if (curIndex1 == -1)
+                break;
         }
-        this->curIndex1--;
     }
     else
     {
-        while (this->array[this->curIndex2]!='\0')
+        curIndex2++;
+        
+        while (array[curIndex2]!='\0')
         {
-            this->curIndex2++;
+            curIndex2++;
+            
+            if (curIndex2 == size)
+                break;
         }
-        this->curIndex2++;
     }
 }
--- MSU_INFO/main.cpp
+++ MSU_INFO/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdio.h>
 #include "OneStack.h"
+#include "TwoStacks.h"
 #include <string>
 #include <fstream>
 #include <algorithm>
@@ -27,7 +28,6 @@ int ex1 ()
     
     if (size<0)
     {
-        cout<<1<<endl;
         fclose (fin);
         fclose (fout);
         return 0;
@@ -92,96 +92,127 @@ int ex1 ()
     return 0;
 }
 
-/*void ex2 ()
- {
- FILE* fin;
- FILE* fout;
- fin = fopen("input.txt", "r");
- fout = fopen("output.txt","w");
- int comand = 0;
- setlocale(LC_CTYPE, "rus");
- 
- 
- fscanf(fin,"%d",&comand);
- 
- int size;
- fscanf(fin,"%d",&size);
- int number;
- TwoStacks stack(size);//ctor
- 
- while(fscanf(fin,"%d",&comand)!=EOF)
- {
- switch (comand)
- {
- case 2:
- {
- stack.stackNull();
- break;
- }
- case 3:
- {
- fscanf(fin,"%d",&number);
- fprintf (fout, "%d\n", stack.isNull(number));
- break;
- }
- case 4:
- {
- string elem;
- fscanf(fin,"%d",&number);
- char curSymbol = '0';
- 
- while(curSymbol!='\n')
- {
- fscanf(fin,"%c",&curSymbol);
- elem+=curSymbol;
- }
- cout<<elem;
- 
- stack.addElement(elem, number);
- break;
- }
- case 5:
- {
- fscanf(fin,"%d",&number);
- string res = stack.getElement(number);
- for (int i=0; i<res.length(); ++i)
- {
- fprintf (fout, "%d\n", res[i]);
- }
- break;
- }
- case 6:
- {
- fscanf(fin,"%d",&number);
- string res = stack.getVertice(number);
- for (int i=0; i<res.length(); ++i)
- {
- fprintf (fout, "%d\n", res[i]);
- }
- break;
- }
- case 7:
- {
- fscanf(fin,"%d",&number);
- stack.deleteVertice(number);
- break;
- }
- case 8:
- {
- fclose (fin);
- fclose (fout);
- break;
- }
- }
- }
- }
- 
- */
+int ex2 ()
+{
+    FILE* fin;
+    FILE* fout;
+    fin = fopen("input.txt", "r");
+    fout = fopen("output.txt","w");
+    int comand = 0;
+    if (fscanf(fin,"%d",&comand) == EOF)
+    {
+        fclose (fin);
+        fclose (fout);
+        
+        return 0;
+    }
+    
+    int size;
+    fscanf(fin,"%d",&size);
+    
+    if (size<0)
+    {
+        fclose (fin);
+        fclose (fout);
+        return 0;
+    }
+    TwoStacks stack(size);
+    
+    int number;
+    
+    while(fscanf(fin,"%d",&comand)!=EOF)
+    {
+        switch (comand)
+        {
+            case 2:
+            {
+                stack.stackNull();
+                break;
+            }
+            case 3:
+            {
+                fscanf(fin,"%d",&number);
+                fprintf (fout, "%d\n", stack.isNull(number));
+                break;
+            }
+            case 4:
+            {
+                string elem;
+                char curSymbol = 'a';
+                fscanf(fin,"%d",&number);
+                
+                char t;
+                fscanf (fin,"%c",&t);
+                
+                fscanf(fin,"%c",&curSymbol);
+                
+                while(curSymbol != '\n')
+                {
+                    elem+=curSymbol;
+                    fscanf(fin,"%c",&curSymbol);
+                }
+                
+                stack.addElement(elem, number);
+                break;
+            }
+            case 5:
+            {
+                try
+                {
+                    fscanf(fin,"%d",&number);
+                    string res = stack.getElement(number);
+                    
+                    cout<<3<<endl;
+                
+                    for (int i=0; i<res.length(); ++i)
+                    {
+                        fprintf (fout, "%c", res[i]);
+                    }
+                    fprintf (fout, "\n");
+                }
+                catch (bool a) {}
+                break;
+            }
+            case 6:
+            {
+                try
+                {
+                    fscanf(fin,"%d",&number);
+                    
+                    string res = stack.getVertice(number);
+                    for (int i=0; i<res.length(); ++i)
+                    {
+                        fprintf (fout, "%c", res[i]);
+                    }
+                    fprintf (fout, "\n");
+                }
+                catch (bool a) {}
+                
+                break;
+            }
+            case 7:
+            {
+                fscanf(fin,"%d",&number);
+                stack.deleteVertice(number);
+                
+                break;
+            }
+            case 8:
+            {
+                fclose (fin);
+                fclose (fout);
+                break;
+            }
+        }
+    }
+    
+    return 0;
+}
 
 
 int main()
 {
-    return ex1();
+    return ex2();
     return 0;
 }
 
