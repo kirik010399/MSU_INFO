a0401538b832cc3268c874bcdebd6d9c8a2cb183
Кирилл Мащенко <kirill010399@mail.ru>
2017-12-05 12:12:30 +0300

    Testing for cos function added

--- MSU_INFO/main.cpp
+++ MSU_INFO/main.cpp
@@ -31,10 +31,33 @@ double sin (double x)
     return res;
 }
 
+double cos (double x)
+{
+    double res = 0, d = 1, fact = 1;
+    int i = 1;
+    
+    while (d/fact>=0.0000000001 || d/fact<=-0.0000000001)
+    {
+        if (i%2==1)
+        {
+            if (i%4==1)
+                res+=d/fact;
+            else if (i%4==3)
+                res-=d/fact;
+        }
+        d*=x;
+        fact*=i;
+        i++;
+    }
+    
+    return res;
+}
+
+
 int main()
 {
     double x;
     cin>>x;
-    cout<<sin(x)<<endl;
+    cout<<sin(x)<<' '<<cos(x)<<endl;
 }
 
