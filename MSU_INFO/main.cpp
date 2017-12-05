cff18fa46edc4d01e37b1d0621ad8a714dc0ad49
Кирилл Мащенко <kirill010399@mail.ru>
2017-12-05 12:09:57 +0300

    testing for sin function added

--- MSU_INFO/main.cpp
+++ MSU_INFO/main.cpp
@@ -8,10 +8,33 @@
 
 using namespace std;
 
+
+double sin (double x)
+{
+    double res = 0, d = 1, fact = 1;
+    int i = 1;
+    
+    while (d/fact>=0.0000000001 || d/fact<=-0.0000000001)
+    {
+        if (i%2!=1)
+        {
+            if (i%4==2)
+                res+=d/fact;
+            else if (i%4==0)
+                res-=d/fact;
+        }
+        d*=x;
+        fact*=i;
+        i++;
+    }
+    
+    return res;
+}
+
 int main()
 {
-    Any a;
-    a = 5; 
-    return 0;
+    double x;
+    cin>>x;
+    cout<<sin(x)<<endl;
 }
 
