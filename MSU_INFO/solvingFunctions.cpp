29f36737ac2833359c02bab2a16d0239e7271b5c
Кирилл Мащенко <kirill010399@mail.ru>
2018-10-12 13:16:50 +0300

    Algorithm of Jordan through all matrix fixed

--- MSU_INFO/solvingFunctions.cpp
+++ MSU_INFO/solvingFunctions.cpp
@@ -9,75 +9,103 @@ int solveSystem(double* matrix, double* vector, double* result, int n)
 {
     int i, j, k;
     double a;
+    int *var;
+    
+    var = new int[n];
     
     int maxStrInd, maxColInd;
 
     double eps = fmax(pow(10, -n*3), 1e-100);
     
-    for (j = 0; j < n; ++j)
+    for (i = 0; i < n; ++i)
+        var[i] = i;
+    
+    for (i = 0; i < n; ++i)
     {
-        a = fabs(matrix[j*n+j]);
-        maxStrInd = j;
-        maxColInd = j;
+        a = fabs(matrix[i*n+i]);
+        maxStrInd = i;
+        maxColInd = i;
         
-        for (i = j; i < n; ++i)
+        for (j = i; j < n; ++j)
         {
-            for (k = j; k < n; ++k)
+            for (k = i; k < n; ++k)
             {
-                if (fabs(matrix[i*n+k]) > a)// Search for max in column
+                if (fabs(matrix[j*n+k]) > a)// Search for max in matrix
                 {
-                    a = matrix[i*n+j];
-                    maxStrInd = i;
-                    maxColInd = j;
+                    a = matrix[j*n+k];
+                    maxStrInd = j;
+                    maxColInd = k;
                 }
             }
         }
         
-        cout<<maxStrInd<<" "<<maxColInd<<endl;
+//        cout<<endl<<maxStrInd<<" "<<maxColInd<<endl;
+//        
+//        for (int p = 0; p < n; ++p)
+//        {
+//            for (int q = 0; q < n; ++q)
+//                cout<<matrix[p*n+q]<<' ';
+//            cout<<' '<<vector[p];
+//            cout<<endl;
+//        }
         
-        for (int p = 0; p < n; ++p)
+        if (fabs(a) < eps)
         {
-            for (int q = 0; q < n ++q)
-                cout<<matrix[p*n+q]<<' ';
-            cout<<endl;
-        }
-        
-        if (fabs(a) < eps) 
+            delete []var;
             return -1;
-        
-        if (maxStrInd != j) // Swap strings (j <-> max)
-        {
-            for (i = j; i < n; ++i)
-                swap(matrix[maxStrInd*n+i], matrix[maxStrInd*n+i]);
-            
-            swap(vector[maxStrInd], vector[j]);
         }
         
+        if (maxStrInd != i) // Swap strings (j <-> max)
+        {
+            for (j = 0; j < n; ++j)
+                swap(matrix[maxStrInd*n+j], matrix[i*n+j]);
+            
+            swap(vector[maxStrInd], vector[i]);
+        }
+//
+//        cout<<endl;
+//        for (int p = 0; p < n; ++p)
+//        {
+//            for (int q = 0; q < n; ++q)
+//                cout<<matrix[p*n+q]<<' ';
+//            cout<<' '<<vector[p];
+//            cout<<endl;
+//        }
+        
+        swap(var[i], var[maxColInd]);//swap variables
+
         if (maxColInd != j) // Swap columns (j <-> max)
         {
-            for (i = j; i < n; ++i)
-                swap(matrix[i*n+maxColInd], matrix[i*n+j]);
+            for (j = 0; j < n; ++j)
+                swap(matrix[j*n+maxColInd], matrix[j*n+i]);
         }
-    }
-    
-    for (j = 0; j < n; ++j)
-    {
-        for(i = 0; i < n; ++i)
+        
+//        cout<<endl;
+//        for (int p = 0; p < n; ++p)
+//        {
+//            for (int q = 0; q < n; ++q)
+//                cout<<matrix[p*n+q]<<' ';
+//            cout<<' '<<vector[p];
+//            cout<<endl;
+//        }
+        
+        for(j = 0; j < n; ++j) // Subtraction from all lines
         {
-            if (i != j && fabs(matrix[i*n+j]) > eps)
+            if (j != i)
             {
-                a = matrix[i*n+j]/matrix[j*n+j];
+                a = matrix[j*n+i]/matrix[i*n+i];
                 
-                for(k = j; k < n; ++k)
-                    matrix[i*n+k] -= a*matrix[j*n+k];
+                for(k = i; k < n; ++k)
+                    matrix[j*n+k] -= a*matrix[i*n+k];
                 
-                vector[i] -= a*vector[j];
+                vector[j] -= a*vector[i];
             }
         }
     }
     
     for (i = 0; i < n; ++i)
-        result[i] = vector[i]/matrix[i*n+i];
+        result[var[i]] = vector[i]/matrix[i*n+i];
     
+    delete []var;
     return 0;
 }
