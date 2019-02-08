95788990a7cb75316d4b6eca142b8df235d386f4
Кирилл Мащенко <kirill010399@mail.ru>
2019-02-08 12:29:08 +0300

    codestyle

--- MSU_INFO/MatFun.hpp
+++ MSU_INFO/MatFun.hpp
@@ -6,7 +6,7 @@
 double func(int i, int j, int n);
 int enterData(double* a, double* b, int n, FILE* fin);
 void printResult(double* a, int n, int m);
-double residualNorm(double* a, double* b, double* x, int n);
+double inaccuracyNorm(double* a, double* b, double* x, int n);
 double errorNorm(double *x, int n);
 
 #endif /* MatFun_hpp */
