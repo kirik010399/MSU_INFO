f373652d65b98db8f54c9297e201915f03947d81
Кирилл Мащенко <kirill010399@mail.ru>
2019-02-08 14:30:17 +0300

    fixed bug

--- MSU_INFO/main.cpp
+++ MSU_INFO/main.cpp
@@ -266,7 +266,7 @@ int main(int argc, char **argv)  {
     cout<<"____Норма погрешности: "<<residualNorm(a, b, x, n)<<endl;
     
     if (type == 2)
-        cout<<"____Норма ошибки: "<<errorNorm(x, n)<<endl; ;
+        cout<<"____Норма ошибки: "<<errorNorm(x, n)<<endl;
     
     cout<<"____Время решения  = "<<t<<" секунд"<<endl;
     
