a88d5541ea013d28d8933463dbc67ed67414897e
Кирилл Мащенко <kirill010399@mail.ru>
2019-09-05 15:33:48 +0300

    First v2.0 program

--- MSU_INFO/main.c
+++ MSU_INFO/main.c
@@ -4,20 +4,21 @@ int main(void)
 {
     double sum1 = 0;
     
-    for (int i = 1; i <= 1000; ++i)
+    for (int i = 2; i <= 1000; ++i)
     {
-        int temp = i*i;
+        int temp = (i-1)*(i+1);
         sum1 += 1.0/temp;
     }
     
     double sum2 = 0;
-    for (int i = 1000; i>=1; --i)
+    for (int i = 1000; i>=2; --i)
     {
-        int temp = i*i;
+        int temp = (i-1)*(i+1);
         sum2 += 1.0/temp;
     }
     
-    printf("%.16lf\n%.16lf\n", sum1, sum2);
+    double res = 3001*1.0/4004;
+    printf("Correct: %.16lf\nLeft->Right: %.16lf\nRight->left: %.16lf\n", res, sum1, sum2);
     
     return 0;
 }
