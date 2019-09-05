9ebaf08483f509a8a1fb2096515feb4fd38f6f4d
Кирилл Мащенко <kirill010399@mail.ru>
2019-09-05 16:11:20 +0300

    other 2

--- MSU_INFO/main.cpp
+++ MSU_INFO/main.cpp
@@ -1,25 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
-double f(double x)
+long double f(double x)
 {
-    return (x-10*10*10)*(x-1/10*1/10*1/10)
+    return (x-1000)*(x-0.001);
 }
 
 int main()
 {
-    long double a = 1, b = 10*10*10, c = 1;
-    
-    b += 1.0/(10*10*10);
+    long double a = 1, b = -1000.001, c = 1;
     
     long double d = b*b-4*a*c;
-    long double x1 = (-b+sqrt(d))/2;
-    long double x2 = (-b-sqrt(d))/2;
+    double x1 = (b*b-d)/(2*a*(-b+sqrt(d)));
+    double x2 = (b*b-d)/(2*a*(-b-sqrt(d)));
     
-//    x1<<2;
-//    x2<<2;
-
-    printf("%.16Lf %.16Lf\n", x1, x2);
+    printf("%.16lf %.16Lf\n%.16lf %.16Lf\n", x1, f(x1), x2, f(x2));
     return 0;
 }
 
