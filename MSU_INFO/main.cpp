5fdfa97f287d091a459996a6728aabd39f42fd79
Кирилл Мащенко <kirill010399@mail.ru>
2019-10-19 18:46:19 +0300

    2d program init

--- MSU_INFO/main.cpp
+++ MSU_INFO/main.cpp
@@ -7,124 +7,78 @@
 
 using namespace std;
 
-vector <double> f2c(vector <double> y, int n)
+void f2cFinal(vector <vector <double> > &c, vector <vector <double> > &y, int n)
 {
     double h = 1.0/n;
-    vector <double> c;
-    
-    c.push_back(0);
-    
-    for (int k = 1; k <= n-1; ++k)
+    double h1 = 1.0/(n-1);
+    int n1 = n + 1;
+        
+    for (int i = 0; i < n1; ++i)
     {
-        double sum = 0;
-        
-        for (int i = 1; i <= n-1; ++i)
-            sum += y[i] * sin(M_PI*k*i*h) * h;
-        
-        double sum1 = 0;
-        
-        for (int i = 1; i <= n-1; ++i)
-            sum1 += sin(M_PI*k*i*h) * sin(M_PI*k*i*h) * h;
-        
-        c.push_back(sum/sum1);
+        c[i][0] = 0;
+        c[0][i] = 0;
+        c[i][n1-1] = 0;
+        c[n1-1][i] = 0;
     }
-    
-    c.push_back(0);
-    
-    return c;
-}
-
-vector <double> c2f(vector <double> c, int n)
-{
-    double h = 1.0/n;
-    vector <double> y;
-    
-    y.push_back(0);
     
     for (int i = 1; i <= n-1; ++i)
     {
-        double sum = 0;
-        
-        for (int m = 0; m <= n-1; ++m)
+        for (int j = 1; j <= n-1; ++j)
         {
-            sum += c[m] * sin(M_PI*m*i*h);
+            double sum = 0;
+            
+            for (int k = 1; k <= n-1; ++k)
+            {
+                for (int p = 1; p <= n-1; ++p)
+                {
+                    sum += y[k][p] * sin(M_PI*i*k*h) * h * sin(M_PI*(p*h1-h1/2)*(2*j-1)*0.5) * h1;
+                }
+            }
+            
+            double sum1 = 0;
+            
+            for (int k = 1; k <= n-1; ++k)
+            {
+                for (int p = 1; p <= n-1; ++p)
+                {
+                    sum1 += sin(M_PI*i*k*h) * sin(M_PI*i*k*h) * h * sin(M_PI*(p*h1-h1/2)*(2*j-1)*0.5) * sin(M_PI*(p*h1-h1/2)*(2*j-1)*0.5) * h1;
+                }
+            }
+            
+            c[i][j] = sum/sum1;
         }
-        
-        y.push_back(sum);
     }
-    
-    y.push_back(0);
-    
-    return y;
 }
 
-vector <double> f2cWithShift(vector <double> y, int n)
+void c2fFinal(vector <vector <double> > &c, vector <vector <double> > &y, int n)
 {
-    double h = 1.0/(n-1);
-    
-    vector <double> c;
-    
-    c.push_back(0);
-    
-//    double tempSum = 0;
-//
-//    for (int i = 1; i <= n-1; ++i)
-//        tempSum += sin(M_PI*(i*h-h/2)*(2*1-1)*0.5) * sin(M_PI*(i*h-h/2)*(2*2-1)*0.5);
-//
-//    printf("%lf\n", tempSum);
-    
-    for (int k = 1; k <= n-1; ++k)
-    {
-        double sum = 0;
-        
-        for (int i = 1; i <= n-1; ++i)
-            sum += y[i] * sin(M_PI*(i*h-h/2)*(2*k-1)*0.5) * h;
-        
-        double sum1 = 0;
-        
-        for (int i = 1; i <= n-1; ++i)
-            sum1 += sin(M_PI*(i*h-h/2)*(2*k-1)*0.5) * sin(M_PI*(i*h-h/2)*(2*k-1)*0.5) * h;
-                
-        c.push_back(sum/sum1);
-    }
-    
-    c.push_back(0);
-    
-    return c;
-}
+    double h = 1.0/n;
+    double h1 = 1.0/(n-1);
+    int n1 = n + 1;
 
-vector <double> c2fWithShift(vector <double> c, int n)
-{
-    double h = 1.0/(n-1);
-    vector <double> y;
-        
-    for (int i = 1; i <= n-1; ++i)
+    for (int i = 0; i < n1; ++i)
     {
-        double sum = 0;
-        
-        for (int m = 0; m <= n-1; ++m)
+        for (int j = 0; j < n1; ++j)
         {
-            sum += c[m] * sin(M_PI*(i*h-h/2)*(2*m-1)*0.5);
+            double sum = 0;
+            
+            for (int k = 0; k < n1; ++k)
+            {
+                for (int p = 1; p < n1; ++p)
+                {
+                    sum += c[k][p] * sin(M_PI*i*k*h) * sin(M_PI*(p*h1-h1/2)*(2*j-1)*0.5);
+                }
+            }
+            
+            y[i][j] = sum;
         }
-        
-        if (i == 1)
-            y.push_back(-sum);
-        
-        y.push_back(sum);
-
-        if (i == n-1)
-            y.push_back(sum);
     }
-        
-    return y;
 }
 
 int main()
 {
     FILE *fin, *fin1, *fout;
     int n;
-    double a;
-    vector <double> c, y, y1;
     
     fin = fopen("input.txt", "r");
     fin1 = fopen("input1.txt", "r");
@@ -132,27 +86,55 @@ int main()
     
     fscanf(fin, "%d", &n);
     
+    vector <vector <double> > c;
+    vector <vector <double> > y;
+    vector <vector <double> > y1;
+    
     for (int i = 0; i < n + 1; ++i)
     {
-        fscanf(fin1, "%lf", &a);
-        y.push_back(a);
+        vector<double> tempC;
+        vector<double> tempY;
+        vector<double> tempY1;
+
+        for (int j = 0; j < n + 1; ++j)
+        {
+            double a;
+            fscanf(fin1, "%lf", &a);
+            tempY.push_back(a);
+            tempC.push_back(0);
+            tempY1.push_back(0);
+        }
+        
+        y.push_back(tempY);
+        c.push_back(tempC);
+        y1.push_back(tempY1);
     }
     
-    c = f2cWithShift(y, n);
+    f2cFinal(c, y, n);
     
     for (int i = 0; i < n + 1; ++i)
     {
-        fprintf(fout, "%f ", c[i]);
+        for (int j = 0; j < n + 1; ++j)
+        {
+            fprintf(fout, "%f ", c[i][j]);
+        }
+        fprintf(fout, "\n");
     }
     
     fprintf(fout, "\n");
     
-    y1 = c2fWithShift(c, n);
+    c2fFinal(c, y1, n);
     
     for (int i = 0; i < n + 1; ++i)
     {
-        fprintf(fout, "%f ", y1[i]);
+        for (int j = 0; j < n + 1; ++j)
+        {
+            fprintf(fout, "%f ", y1[i][j]);
+        }
+        fprintf(fout, "\n");
     }
+       
+    fprintf(fout, "\n");
     
     fclose(fin);
     fclose(fout);
--- MSU_INFO/sample1.cpp
+++ MSU_INFO/sample1.cpp
@@ -1,68 +1,51 @@
-//#include <stdio.h>
-//#include <math.h>
-//#include <time.h>
-//#include <iostream>
-//#include <vector>
-//#include <algorithm>
-//
-//using namespace std;
-//
-//double f (double x)
-//{
-//    return x*(x-1);
-//}
-//
-//void generatePoints(FILE *fout, int n)
-//{
-//    double a = 0;
-//    double b = 1;
-//    
-//    double dt = fabs(b-a)/(n);
-//    
-//    for (double i = a; i <= b; i+=dt)
-//    {
-//        fprintf(fout, "%f ", f(i));
-//    }
-//}
-//
-//void generatePointsWithDoubleShift(FILE *fout, int n)
-//{
-//    double a = 0;
-//    double b = 1;
-//    
-//    double dt = fabs(b-a)/(n);
-//    double newDt = (1.0 + dt)/n;
-//
-//    fprintf(fout, "%f ", -f(-dt/2 + newDt));
-//    
-////    printf("%f %f\n", dt, newDt);
-////    printf("%f ", -dt/2);
-//    
-//    for (double i = -dt/2 + newDt; i <= 1+dt/2-newDt; i+=newDt)
-//    {
-////        printf("%f ", i);
-//        fprintf(fout, "%f ", f(i));
-//    }
-//    
-////    printf("%f/n", 1+dt/2);
-//
-//    fprintf(fout, "%f ", f(1+dt/2-newDt));
-//}
-//
-//int main(void)
-//{
-//    FILE *fin, *fout;
-//    int n;
-//    
-//    fin = fopen("input.txt", "r");
-//    fout = fopen("input1.txt", "w");
-//    
-//    fscanf(fin, "%d", &n);
-//    
-//    generatePointsWithDoubleShift(fout, n);
-//    
-//    fclose(fin);
-//    fclose(fout);
-//}
-//
-//
+#include <stdio.h>
+#include <math.h>
+#include <time.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+double f (double x, double y)
+{
+//    return sin(M_PI*x) * sin(M_PI*(p*h1-h1/2)*(2*j-1)*0.5);
+}
+
+void generatePointsFinal(FILE *fout, int n)
+{
+    double a = 0, b = 1;
+    
+    double dt = fabs(b-a)/(n);
+    double newDt = (1.0 + dt)/n;
+    
+    for (double j = a; j <= b; j+=dt)
+    {
+        fprintf(fout, "%f ", -f(-dt/2 + newDt, j));
+        
+        for (double i = -dt/2 + newDt; i <= 1+dt/2-newDt; i+=newDt)
+        {
+            fprintf(fout, "%f ", f(i, j));
+        }
+        
+        fprintf(fout, "%f\n", f(1+dt/2-newDt, j));
+    }
+}
+
+int main(void)
+{
+    FILE *fin, *fout;
+    int n;
+    
+    fin = fopen("input.txt", "r");
+    fout = fopen("input1.txt", "w");
+    
+    fscanf(fin, "%d", &n);
+    
+    generatePointsFinal(fout, n);
+    
+    fclose(fin);
+    fclose(fout);
+}
+
+
