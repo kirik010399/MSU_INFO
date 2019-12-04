2b158f5f3a493b59358877f873caa00af8be3217
Кирилл Мащенко <kirill010399@mail.ru>
2019-12-04 20:18:38 +0300

    Heap added

--- MSU_INFO/sample1.cpp
+++ MSU_INFO/sample1.cpp
@@ -3,73 +3,163 @@
 #include <stdlib.h>
 #include <iostream>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-struct tree
+class Heap
 {
-    double value;
-    struct tree *left;
-    struct tree *right;
-    int count;
-};
+public:
+    int *list;
+    int heapSize;
+    int heapMaxSize;
+    
+    Heap()
+    {
+        list = new int[100];
+        heapMaxSize = 100;
+        heapSize = 0;
+    }
+    
+    Heap(int size)
+    {
+        list = new int[size];//current - i, left child - 2*i+1, right child - 2*i+2
+        heapMaxSize = size;
+        heapSize = 0;
+    }
+    
+    ~Heap()
+    {
+        delete []list;
+    }
+    
+    void addElement(int value)
+    {
+        list[heapSize] = value;
+        int i = heapSize;
+        int parent = (i-1)/2;
+        
+        while(parent >= 0 && i > 0)
+        {
+            if(list[i] > list[parent])
+            {
+              swap(list[i], list[parent]);
+            }
 
-struct tree* addToTree(struct tree *root, double newValue);
-void printTree(struct tree *p, ofstream &fout);
+            i = parent;
+            parent = (i-1)/2;//if even - then right child and will return parent too
+        }
+        
+        ++heapSize;
+    }
+    
+    void heapify(int i)
+    {
+        int left, right;
+        left = 2*i+1;
+        right = 2*i+2;
+        
+        if(left < heapSize)
+        {
+            if(list[i] < list[left])
+            {
+                swap(list[i], list[left]);
+                heapify(left);
+            }
+        }
+        
+        if(right < heapSize)
+        {
+            if(list[i] < list[right])
+            {
+                swap(list[i], list[right]);
+                heapify(right);
+            }
+        }
+    }
+    
+    void buildHeap(int *a, int n)
+    {
+        heapSize = n;
+        for (int i = 0; i < heapSize; ++i)
+            addElement(a[i]);
+    }
+    
+    void buildHeapOptimal(int *a, int n)
+    {
+        heapSize = n;
+        
+        for(int i = 0; i < heapSize; ++i)
+            list[i] = a[i];
+
+        for (int i = heapSize/2; i >= 0; i--)
+            heapify(i);
+    }
+    
+    int popMax()
+    {
+        int max = list[0];
+        list[0] = list[heapSize-1];
+        --heapSize;
+        return max;
+    }
+    
+    void heapSort(int *a, int n)
+    {
+        buildHeapOptimal(a, n);
+        for (int i = n-1; i >= 0; --i)
+        {
+            a[i] = popMax();
+            heapify(0);
+        }
+    }
+    
+    void outHeap()
+    {
+        int i = 0;
+        int k = 1;
+        while(i < heapSize)
+        {
+            while((i < k) && (i < heapSize))
+            {
+                cout << list[i] << " ";
+                i++;
+            }
+            cout << endl;
+            k = k * 2 + 1;
+        }
+    }
+    
+    void out()
+    {
+        for (int i = 0; i < heapSize; ++i)
+        {
+            cout<<list[i]<<" ";
+        }
+        cout<<endl;
+    }
+};
 
 int main()
 {
-    double value;
-    struct tree *root = NULL;
+    int value;// change here
     ifstream fin("input.txt");
     ofstream fout("output.txt");
-            
+        
+    Heap heap(10);
+    
     while (!fin.eof())
     {
         if (fin>>value)
-            root = addToTree(root, value);
+        {
+            heap.addElement(value);
+        }
     }
     
-    printTree(root, fout);
-    
+    heap.outHeap();
+    cout<<endl;
+    heap.out();
+
     return 0;
 }
 
-struct tree* addToTree(struct tree *root, double newValue)
-{
-    if (root == NULL)
-    {
-        root = (struct tree*)malloc(sizeof(struct tree));
-        
-        root->value = newValue;
-        
-        root->left = NULL;
-        root->right = NULL;
-        root->count = 1;
-        return root;
-    }
-    
-    double curValue = root->value;
-    
-    if (curValue < newValue)
-        root->right = addToTree(root->right, newValue);
-    else if (curValue > newValue)
-        root->left = addToTree(root->left, newValue);
-    else
-        ++root->count;
-    
-    return root;
-}
-
-void printTree(struct tree *p, ofstream &fout)
-{
-    if(p != NULL)
-    {
-        printTree(p->left, fout);
-        
-        for (int i = 0; i < p->count; ++i)
-            fout<<p->value<<" ";
-        
-        printTree(p->right, fout);
-    }
-}
--- MSU_INFO/sample2.cpp
+++ MSU_INFO/sample2.cpp
@@ -1,75 +1,75 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <iostream>
-#include <fstream>
-
-using namespace std;
-
-struct tree
-{
-    string value;// change here
-    struct tree *left;
-    struct tree *right;
-    int count;
-};
-
-struct tree* addToTree(struct tree *root, string newValue);// change here
-void printTree(struct tree *p, ofstream &fout);
-
-int main()
-{
-    string value;// change here
-    struct tree *root = NULL;
-    ifstream fin("input.txt");
-    ofstream fout("output.txt");
-            
-    while (!fin.eof())
-    {
-        if (fin>>value)
-            root = addToTree(root, value);
-    }
-    
-    printTree(root, fout);
-    
-    return 0;
-}
-
-struct tree* addToTree(struct tree *root, string newValue)// change here
-{
-    if (root == NULL)
-    {
-        root = (struct tree*)malloc(sizeof(struct tree));
-        
-        root->value = newValue;
-        
-        root->left = NULL;
-        root->right = NULL;
-        root->count = 1;
-        return root;
-    }
-    
-    string curValue = root->value;// change here
-    
-    if (curValue < newValue)
-        root->right = addToTree(root->right, newValue);
-    else if (curValue > newValue)
-        root->left = addToTree(root->left, newValue);
-    else
-        ++root->count;
-    
-    return root;
-}
-
-void printTree(struct tree *p, ofstream &fout)
-{
-    if(p != NULL)
-    {
-        printTree(p->left, fout);
-        
-        for (int i = 0; i < p->count; ++i)
-            fout<<p->value<<" ";
-        
-        printTree(p->right, fout);
-    }
-}
+//#include <stdio.h>
+//#include <string.h>
+//#include <stdlib.h>
+//#include <iostream>
+//#include <fstream>
+//
+//using namespace std;
+//
+//struct tree
+//{
+//    string value;// change here
+//    struct tree *left;
+//    struct tree *right;
+//    int count;
+//};
+//
+//struct tree* addToTree(struct tree *root, string newValue);// change here
+//void printTree(struct tree *p, ofstream &fout);
+//
+//int main()
+//{
+//    string value;// change here
+//    struct tree *root = NULL;
+//    ifstream fin("input.txt");
+//    ofstream fout("output.txt");
+//            
+//    while (!fin.eof())
+//    {
+//        if (fin>>value)
+//            root = addToTree(root, value);
+//    }
+//    
+//    printTree(root, fout);
+//    
+//    return 0;
+//}
+//
+//struct tree* addToTree(struct tree *root, string newValue)// change here
+//{
+//    if (root == NULL)
+//    {
+//        root = (struct tree*)malloc(sizeof(struct tree));
+//        
+//        root->value = newValue;
+//        
+//        root->left = NULL;
+//        root->right = NULL;
+//        root->count = 1;
+//        return root;
+//    }
+//    
+//    string curValue = root->value;// change here
+//    
+//    if (curValue < newValue)
+//        root->right = addToTree(root->right, newValue);
+//    else if (curValue > newValue)
+//        root->left = addToTree(root->left, newValue);
+//    else
+//        ++root->count;
+//    
+//    return root;
+//}
+//
+//void printTree(struct tree *p, ofstream &fout)
+//{
+//    if(p != NULL)
+//    {
+//        printTree(p->left, fout);
+//        
+//        for (int i = 0; i < p->count; ++i)
+//            fout<<p->value<<" ";
+//        
+//        printTree(p->right, fout);
+//    }
+//}
