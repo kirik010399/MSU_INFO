e9e224338f7313e960c604958a913ef7df80ec5c
Кирилл Мащенко <kirill010399@mail.ru>
2019-12-11 12:47:50 +0300

    Balanced Binary tree

--- MSU_INFO/main.cpp
+++ MSU_INFO/main.cpp
@@ -9,27 +9,20 @@ using namespace std;
 
 int main()
 {
+    double value;
+    struct tree *root = NULL;
+    ifstream fin("input.txt");
     ofstream fout("output.txt");
-
-    tree *root = new tree (1);
-    root->left = new tree (2);
-    root->right = new tree (3);
-    root->left->left = new tree(4);
-    root->left->right = new tree(5);
-    root->right->right = new tree(6);
-    root->left->right->left = new tree(7);
-    root->left->right->right = new tree(8);
-    root->right->right->left = new tree(9);
+            
+    while (!fin.eof())
+    {
+        if (fin>>value)
+            root = addToTree(root, value);
+    }
     
-    printTreeStraight(root, fout);
-    fout<<endl;
     printTreeCenter(root, fout);
-    fout<<endl;
-    printTreeReverse(root, fout);
-    fout<<endl;
-    printTreeWide(root, fout);
-    fout<<endl;
     
     return 0;
 }
+
 
--- MSU_INFO/tree.hpp
+++ MSU_INFO/tree.hpp
@@ -16,28 +16,93 @@ public:
         this->value = value;
         this->left = NULL;
         this->right = NULL;
+        this->height = 1;
     }
     ~tree(){}
 
     double value;
+    int height;
     tree *left;
     tree *right;
 };
 
-void printTreeStraight(tree *p, ofstream &fout);
-void printTreeCenter(tree *p, ofstream &fout);
-void printTreeReverse(tree *p, ofstream &fout);
-void printTreeWide(tree *p, ofstream &fout);
+int getHeight(tree *p);
+int getBalanceFactor(tree *p);
+void setCorrectHeight(tree *p);
 
-void printTreeStraight(tree *p, ofstream &fout)
+int getHeight(tree *p)
 {
-    if(p != NULL)
-    {
-        fout<<p->value<<" ";
+    if (!p)
+        return 0;
+    else
+        return p->height;
+}
 
-        printTreeStraight(p->left, fout);
-        printTreeStraight(p->right, fout);
+int getBalanceFactor(tree *p)
+{
+    return getHeight(p->right) - getHeight(p->left);
+}
+
+void setCorrectHeight(tree *p)
+{
+    int leftHeight = getHeight(p->left);
+    int rightHeight = getHeight(p->right);
+    
+    if (leftHeight > rightHeight)
+        p->height = leftHeight + 1;
+    else
+        p->height = rightHeight + 1;
+}
+
+tree* rotateRight(tree *p)
+{
+    tree *q = p->left;
+    p->left = q->right;
+    q->right = p;
+    setCorrectHeight(p);
+    setCorrectHeight(q);
+    return q;
+}
+
+tree* rotateLeft(tree *q)
+{
+    tree *p = q->right;
+    q->right = p->left;
+    p->left = q;
+    setCorrectHeight(q);
+    setCorrectHeight(p);
+    return p;
+}
+
+tree* balance(tree *p)
+{
+    setCorrectHeight(p);
+    
+    if (getBalanceFactor(p) == 2)
+    {
+        if (getBalanceFactor(p->right) < 0)
+            p->right = rotateRight(p->right);
+        return rotateLeft(p);
     }
+    if (getBalanceFactor(p) == -2)
+    {
+        if (getBalanceFactor(p->left) > 0)
+            p->left = rotateLeft(p->left);
+        return rotateRight(p);
+    }
+    return p;
+}
+
+tree *addToTree(tree* p, double newValue)
+{
+    if (!p)
+        return new tree(newValue);
+    if (newValue < p->value)
+        p->left = addToTree(p->left, newValue);
+    else
+        p->right = addToTree(p->right, newValue);
+    
+    return balance(p);
 }
 
 void printTreeCenter(tree *p, ofstream &fout)
@@ -45,40 +110,8 @@ void printTreeCenter(tree *p, ofstream &fout)
     if(p != NULL)
     {
         printTreeCenter(p->left, fout);
-
-        fout<<p->value<<" ";
-
+        fout<<p->value<<" "<<p->height<<endl;
         printTreeCenter(p->right, fout);
-    }
-}
-
-void printTreeReverse(tree *p, ofstream &fout)
-{
-    if(p != NULL)
-    {
-        printTreeReverse(p->left, fout);
-        printTreeReverse(p->right, fout);
-        
-        fout<<p->value<<" ";
-    }
-}
-
-void printTreeWide(tree *p, ofstream &fout)
-{
-    queue <tree*> q;
-    q.push(p);
-    
-    while (!q.empty())
-    {
-        tree* elem = q.front();
-        q.pop();
-        
-        fout<<elem->value<<" ";
-        
-        if (elem->left)
-            q.push(elem->left);
-        if (elem->right)
-            q.push(elem->right);
     }
 }
 
