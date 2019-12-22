fe7324ca75c0374a1570fbb515b287175d948982
Кирилл Мащенко <kirill010399@mail.ru>
2019-12-22 20:16:04 +0300

    Initial

--- MSU_INFO/main.cpp
+++ MSU_INFO/main.cpp
@@ -5,98 +5,59 @@
 #include <vector>
 #include <algorithm>
 
-double func(double x)
+double func(double x, double y)
 {
-    return x * sin(x);
+    return x * sin(y);
 }
 
-double integrateFunc(double x)
+double realResult()
 {
-    return sin(x) - x*cos(x);
+    double a = 0;
+    double b = 1;
+    
+    return (b*b/2 - a*a/2)*(cos(a) - cos(b));
 }
 
-double integrateNewton(double a, double b, int n)
+void generateTriangles(FILE *fout, int n)
 {
-    double sum = 0;
-    double dx = (b-a)/n;
     
-    for (int i = 0; i < n; ++i)
-    {
-        double a0 = a+dx*i;
-        double b0 = a+dx*(i+1);
-        
-        double dx0 = (b0 - a0)/3;
-        
-        double tempX0 = a0;
-        double tempX1 = a0 + dx0;
-        double tempX2 = a0 + 2*dx0;
-        double tempX3 = a0 + 3*dx0;
-        
-        double c0 = (b0-a0)/8;
-        double c1 = 3*(b0-a0)/8;
-        double c2 = 3*(b0-a0)/8;
-        double c3 = (b0-a0)/8;
-                
-        sum += c0 * func(tempX0) + c1 * func(tempX1) + c2 * func(tempX2) + c3 * func(tempX3);
-    }
-    
-    return sum;
 }
 
-double integrateGauss(double a, double b, int n)
-{
-    double sum = 0;
-    double dx = (b-a)/n;
-    
-    double x0 = -sqrt(3.0/7+2*sqrt(6.0/5)/7);
-    double x1 = -sqrt(3.0/7-2*sqrt(6.0/5)/7);
-    double x2 = -x1;
-    double x3 = -x0;
-        
-    for (int i = 0; i < n; ++i)
-    {
-        double a0 = a+dx*i;
-        double b0 = a+dx*(i+1);
-        
-        double tempX0 = (b0 - a0) * x0/2 + (b0 + a0)/2;
-        double tempX1 = (b0 - a0) * x1/2 + (b0 + a0)/2;
-        double tempX2 = (b0 - a0) * x2/2 + (b0 + a0)/2;
-        double tempX3 = (b0 - a0) * x3/2 + (b0 + a0)/2;
-        
-        double c1 = (b0 - a0) * ((b0*b0 + a0*b0 + a0*a0)/3 - (tempX0*tempX0 + tempX3*tempX3)/2) / (tempX1*tempX1 + tempX2*tempX2 - tempX0*tempX0 - tempX3*tempX3);
-        double c0 = (b0 - a0)/2 - c1;
-        double c2 = c1;
-        double c3 = c0;
-        
-        sum += c0*func(tempX0) + c1*func(tempX1) + c2*func(tempX2) + c3*func(tempX3);
-    }
-    
-    return sum;
-}
+double integrateTriangles(){return 0;}
 
 int main()
 {
-    double a, b;
-    int n;
-    FILE *fin, *foutNewton, *foutGauss;
+    int beginN, endN;
+    FILE *fin, *foutTriangles, *finTriangles, *fout;
     
     fin = fopen("input.txt", "r");
-    foutNewton = fopen("outputNewton.txt", "w");
-    foutGauss = fopen("outputGauss.txt", "w");
-
-    fscanf(fin, "%lf %lf %n", &a, &b, &n);
+    fout = fopen("output.txt", "w");
     
-    printf("Newton method: %.16lf\n", integrateNewton(a, b, n));
-    printf("Gauss method: %.16lf\n", integrateGauss(a, b, n));
+    fscanf(fin, "%d %d", &beginN, &endN);
     
-    double realResult = integrateFunc(b) - integrateFunc(a);
-    printf("Real integral: %.16lf\n", realResult);
-    
-    for (int i = 1; i <= 100; ++i)
+    for (int i = beginN; i <= endN; ++i)
     {
-        fprintf(foutNewton, "%.16lf, ", realResult - integrateNewton(a, b, i));
-        fprintf(foutGauss, "%.16lf, ", realResult - integrateGauss(a, b, i));
+        foutTriangles = fopen("triangles.txt", "w");
+        generateTriangles(foutTriangles, i);
+        fclose(foutTriangles);
+        
+        finTriangles = fopen("triangles.txt", "r");
+        
+        int n;
+        fscanf(finTriangles, "%d", &n);
+        
+        //
+        
+        double result = integrateTriangles();
+        
+        printf("%.16lf\n", result);
+        fprintf(fout, "%.16lf, ", fabs(realResult() - result));
+        
+        fclose(finTriangles);
     }
+    
+    fclose(fin);
+    fclose(fout);
     
     return 0;
 }
