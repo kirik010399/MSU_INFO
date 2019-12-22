942b75b69827199308795596005e0b744468a2d6
Кирилл Мащенко <kirill010399@mail.ru>
2019-12-22 19:40:49 +0300

    Initial commit

--- MSU_INFO/main.cpp
+++ MSU_INFO/main.cpp
@@ -1,279 +1,102 @@
+#include <iostream>
 #include <stdio.h>
 #include <math.h>
 #include <time.h>
-#include <iostream>
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
-double f (double x, double y)
+double func(double x)
 {
-    return y*(y-1)*(2*x*x*x - 3*x*x);
+    return x*x;
 }
 
-double getCompY(int i, int k, int n)//i - number of vector, k - number of component
+double integrateFunc(double x)
 {
-    double h = 1.0/n;
-    return sin(M_PI*i*k*h);
+    return x*x*x/3;
 }
 
-double getCompX(int j, int p, int n)//j - number of vector, p - number of component
+double integrateNewton(double a, double b, int n)
 {
-    double h1 = 1.0/(n-1);
-    return sin(M_PI*(p*h1-h1/2)*(2*j-1)*0.5);
-}
-
-double getBasisY(int i, double k)//i - number of vector, k - double y
-{
-    return sin(M_PI*i*k);
-}
-
-double getBasisX(int j, double p)//j - number of vector, p - double x
-{
-    return sin(M_PI*p*(2*j-1)*0.5);
-}
-
-void f2cFinal(vector <vector <double> > &c, vector <vector <double> > &y, vector <vector <double> > &d, int n)
-{
-    double h = 1.0/n;
-    double h1 = 1.0/(n-1);
-    int n1 = n + 1;
-        
-    for (int i = 0; i < n1; ++i)
-    {
-        c[i][0] = 0;
-        c[0][i] = 0;
-        c[i][n1-1] = 0;
-        c[n1-1][i] = 0;
-    }
+    double sum = 0;
+    double dx = (b-a)/n;
     
-    for (int i = 1; i <= n-1; ++i)
+    for (int i = 0; i < n; ++i)
     {
-        for (int j = 1; j <= n-1; ++j)
-        {
-            double sum = 0;
-            double sum1 = 0;
-
-            for (int k = 1; k <= n-1; ++k)
-            {
-                double compY = getCompY(i, k, n);
+        double a0 = a+dx*i;
+        double b0 = a+dx*(i+1);
+        
+        double dx0 = (b0 - a0)/3;
+        
+        double tempX0 = a0;
+        double tempX1 = a0 + dx0;
+        double tempX2 = a0 + 2*dx0;
+        double tempX3 = a0 + 3*dx0;
+        
+        double c0 = (b0-a0)/8;
+        double c1 = 3*(b0-a0)/8;
+        double c2 = 3*(b0-a0)/8;
+        double c3 = (b0-a0)/8;
                 
-                sum += y[k][j] * compY * h;
-                sum1 += compY * compY * h;
-            }
-            
-            d[i][j] = sum/sum1;
-        }
+        sum += c0*func(tempX0) + c1*func(tempX1) + c2*func(tempX2) + c3*func(tempX3);
     }
     
-    for (int i = 1; i <= n-1; ++i)
-    {
-        for (int j = 1; j <= n-1; ++j)
-        {
-            double sum = 0;
-            double sum1 = 0;
-
-            for (int p = 1; p <= n-1; ++p)
-            {
-                double compX = getCompX(j, p, n);
-                
-                sum += d[i][p] * compX * h1;
-                sum1 += compX * compX * h1;
-            }
-            
-            c[i][j] = sum/sum1;
-        }
-    }
+    return sum;
 }
 
-void c2fFinal(vector <vector <double> > &c, vector <vector <double> > &y, int n)
+double integrateGauss(double a, double b, int n)
 {
-    int n1 = n + 1;
-
-    for (int i = 0; i < n1; ++i)
+    double sum = 0;
+    double dx = (b-a)/n;
+    
+    double x0 = -sqrt(3.0/7+2*sqrt(6.0/5)/7);
+    double x1 = -sqrt(3.0/7-2*sqrt(6.0/5)/7);
+    double x2 = -x1;
+    double x3 = -x0;
+        
+    for (int i = 0; i < n; ++i)
     {
-        for (int j = 0; j < n1; ++j)
-        {
-            double sum = 0;
-            
-            for (int k = 0; k < n1; ++k)
-            {
-                for (int p = 0; p < n1; ++p)
-                {
-                    sum += c[k][p] * getCompY(i, k, n) * getCompX(j, p, n);
-                }
-            }
-            
-            y[i][j] = sum;
-        }
-    }
-}
-
-void residual(vector <vector <double> > c, int n)
-{
-    FILE *fout;
-    fout = fopen("output1.txt", "w");
-    
-    double dt = 1.0/n;
-    double newDt = 1.0/(n-1);
-    int n1 = n + 1;
-    
-    double maxRes = 0;
-    
-    for (double k = 0; k <= 1 + 1e-10; k+=dt/2)
-    {
-       for (double p = newDt/2; p <= 1-newDt/2 + 1e-10; p+=newDt/2)
-        {
-            double tempValue = 0;
-                    
-            for (int i = 0; i < n1; ++i)
-            {
-                for (int j = 0; j < n1; ++j)
-                {
-                    tempValue += c[i][j] * getBasisY(i, k) * getBasisX(j, p);
-                }
-            }
-            
-            double tempRes = fabs(f(p, k) - tempValue);
-            
-            fprintf(fout, "%f ", tempRes);
-            
-            if(tempRes > maxRes)
-                maxRes = tempRes;
-        }
-        fprintf(fout, "\n");
+        double a0 = a+dx*i;
+        double b0 = a+dx*(i+1);
+        
+        double tempX0 = (b0 - a0) * x0/2 + (b0 + a0)/2;
+        double tempX1 = (b0 - a0) * x1/2 + (b0 + a0)/2;
+        double tempX2 = (b0 - a0) * x2/2 + (b0 + a0)/2;
+        double tempX3 = (b0 - a0) * x3/2 + (b0 + a0)/2;
+        
+        double c1 = (b0 - a0) * ((b0*b0 + a0*b0 + a0*a0)/3 - (tempX0*tempX0 + tempX3*tempX3)/2) / (tempX1*tempX1 + tempX2*tempX2 - tempX0*tempX0 - tempX3*tempX3);
+        double c0 = (b0 - a0)/2 - c1;
+        double c2 = c1;
+        double c3 = c0;
+        
+        sum += c0*func(tempX0) + c1*func(tempX1) + c2*func(tempX2) + c3*func(tempX3);
     }
     
-    printf("%.16lf\n", maxRes);
-}
-
-void generatePointsFinal(FILE *fout, int n)
-{
-    double dt = 1.0/n;
-    double newDt = 1.0/(n-1);
-        
-    int k1 = 0, k2 = 0;
-    
-    for (double j = 0; j <= 1 + 1e-10; j+=dt)
-    {
-        k1 = 0;
-        
-        fprintf(fout, "%.16f ", -f(newDt/2, j));
-        
-        for (double i = newDt/2; i <= 1-newDt/2 + 1e-10; i += newDt)
-        {
-            fprintf(fout, "%.16f ", f(i, j));
-            k1++;
-        }
-        
-        fprintf(fout, "%.16f\n", f(1-newDt/2, j));
-        
-        if (k1 != n-1)
-            printf("X error");
-        k2++;
-    }
-    
-    if (k2 != n+1)
-        printf("Y error");
+    return sum;
 }
 
 int main()
 {
-    FILE *fin, *fin1, *fout;
-    int begin, end;
+    double a, b;
+    int n;
+    FILE *fin, *foutNewton, *foutGauss;
     
     fin = fopen("input.txt", "r");
+    foutNewton = fopen("outputNewton.txt", "w");
+    foutGauss = fopen("outputGauss.txt", "w");
+
+    fscanf(fin, "%lf %lf %n", &a, &b, &n);
     
-    fscanf(fin, "%d", &begin);
-    fscanf(fin, "%d", &end);
-
-    for (int n = begin; n <= end; n += 1)
+    printf("Newton method: %.16lf\n", integrateNewton(a, b, n));
+    printf("Gauss method: %.16lf\n", integrateGauss(a, b, n));
+    
+    double realResult = integrateFunc(b) - integrateFunc(a);
+    printf("Real integral: %.16lf\n", realResult);
+    
+    for (int i = 1; i <= 100; ++i)
     {
-        fin1 = fopen("input1.txt", "w");
-        fout = fopen("output.txt", "w");
-        
-        generatePointsFinal(fin1, n);
-        
-        fclose(fin1);
-        fin1 = fopen("input1.txt", "r");
-        
-        vector <vector <double> > c;
-        vector <vector <double> > y;
-        vector <vector <double> > y1;
-        vector <vector <double> > d;
-        
-        for (int i = 0; i < n + 1; ++i)
-        {
-            vector<double> tempC;
-            vector<double> tempY;
-            vector<double> tempY1;
-            vector<double> tempD;
-
-            for (int j = 0; j < n + 1; ++j)
-            {
-                double a;
-                fscanf(fin1, "%lf", &a);
-                tempY.push_back(a);
-                tempC.push_back(0);
-                tempY1.push_back(0);
-                tempD.push_back(0);
-            }
-            
-            y.push_back(tempY);
-            c.push_back(tempC);
-            y1.push_back(tempY1);
-            d.push_back(tempD);
-        }
-        
-        f2cFinal(c, y, d, n);
-        
-        fprintf(fout, "C coeff:\n");
-        
-        for (int i = 0; i < n + 1; ++i)
-        {
-            for (int j = 0; j < n + 1; ++j)
-            {
-                fprintf(fout, "%.16f ", c[i][j]);
-            }
-            fprintf(fout, "\n");
-        }
-        
-        fprintf(fout, "\n");
-        
-        c2fFinal(c, y1, n);
-        
-        residual(c, n);
-        
-        fprintf(fout, "Y result:\n");
-
-        for (int i = 0; i < n + 1; ++i)
-        {
-            for (int j = 0; j < n + 1; ++j)
-            {
-                fprintf(fout, "%.16f ", y1[i][j]);
-            }
-            fprintf(fout, "\n");
-        }
-           
-        fprintf(fout, "\n");
-        
-        fprintf(fout, "Y begin:\n");
-
-        for (int i = 0; i < n + 1; ++i)
-        {
-            for (int j = 0; j < n + 1; ++j)
-            {
-                fprintf(fout, "%.16f ", y[i][j]);
-            }
-            fprintf(fout, "\n");
-        }
-          
-        fprintf(fout, "\n");
-
-        fclose(fin);
-        fclose(fout);
+        fprintf(foutNewton, "%.16lf ", realResult - integrateNewton(a, b, i));
+        fprintf(foutGauss, "%.16lf ", realResult - integrateGauss(a, b, i));
     }
+    
+    return 0;
 }
-
-
